929b58c42f59ac787876e5e5a3aa396e97ff64e5
yu-ki <webmaster@horrorgame.net>
2017-06-09 19:04:09 +0900

    ちびちび作成中

---
+++ sources/web/app/Http/Controllers/Admin/VersionUpController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VersionUpController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+
+    public function database()
+    {
+
+    }
+}
