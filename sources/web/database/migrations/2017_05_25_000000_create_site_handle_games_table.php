963d43d309bf63cf2f664b706d468fc2429e120e
yu-ki <webmaster@horrorgame.net>
2017-06-13 17:44:21 +0900

    マイグレーションが動いた

--- sources/web/database/migrations/2017_05_25_000000_create_site_handle_games_table.php
+++ sources/web/database/migrations/2017_05_25_000000_create_site_handle_games_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSiteDailyAccessesTable extends Migration
+class CreateSiteHandleGamesTable extends Migration
 {
     /**
      * Run the migrations.
