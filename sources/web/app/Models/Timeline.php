8408bb9358b3ccf7c68b21308cbc3168c8fdd581
huckle <webmaster@horrorgame.net>
2017-09-03 19:56:13 +0900

    とりあえずソースだけ用意

---
+++ sources/web/app/Models/Timeline.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * タイムラインモデル
+ */
+
+namespace Hgs3\Models;
+
+use Hgs3\Models\Orm\SiteSearchIndex;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class Timeline
+{
+    public function add()
+    {
+    }
+}
