0c2146c8e0e26f116e7f2bcbc6357641e314ff9b
huckle <webmaster@horrorgame.net>
2017-09-07 08:22:26 +0900

    タイムラインのテキスト取得方針決定

--- sources/web/app/Models/Timeline.php
+++ sources/web/app/Models/Timeline.php
@@ -5,10 +5,36 @@
 
 namespace Hgs3\Models;
 
+use Hgs3\Constants\TimelineText;
+
 class Timeline
 {
     public function add($tltId, $param = [])
     {
 
+    }
+
+
+
+    public function addNewGameSoftText($gameId, $gameName)
+    {
+        $text = TimelineText::getInstance()->getText(TimelineText::TLT_NEW_GAME_SOFT, [
+            url2('game/soft').'/'.$gameId, $gameName
+        ]);
+
+
+
+
+    }
+
+    public function getUpdateGameSoftText($gameId, $gameName)
+    {
+        return sprintf($this->text[self::TLT_UPDATE_GAME_SOFT], url2('game/soft').'/'.$gameId, $gameName);
+    }
+
+
+    private function save()
+    {
+        $client = new \MongoDB\Client("mongodb://localhost:27017");
     }
 }
