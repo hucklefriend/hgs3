8079f2a0b2c8ab7c609c941880438ca95d2c693b
yu-ki <webmaster@horrorgame.net>
2017-09-26 18:08:30 +0900

    新着情報テーブル

---
+++ sources/web/app/Models/Orm/NewInfo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * タイムラインモデル
+ */
+
+namespace Hgs3\Models\Orm;
+
+use Hgs3\Constants\TimelineType;
+use Hgs3\Models\Orm\Game;
+use Hgs3\Models\Orm\Review;
+use Hgs3\Models\Orm\UserCommunity;
+use Hgs3\User;
+
+class NewInfo extends \Eloquent
+{
+
+}
---
+++ sources/web/database/migrations/2017_05_25_000000_create_new_information_table.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 新着情報テーブルの作成
+ */
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NewInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('new_information', function (Blueprint $table) {
+            $table->increments('id')->comment('新着情報ID');
+            $table->unsignedInteger('text_type')->comment('テキスト種類');
+            $table->unsignedInteger('param1')->default(0)->comment('パラメーター1');
+            $table->unsignedInteger('param2')->default(0)->comment('パラメーター2');
+            $table->unsignedInteger('param3')->default(0)->comment('パラメーター3');
+            $table->unsignedInteger('param4')->default(0)->comment('パラメーター4');
+            $table->unsignedInteger('param5')->default(0)->comment('パラメーター5');
+            $table->dateTime('date_time')->comment('日時');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
