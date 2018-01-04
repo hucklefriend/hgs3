f57a921768ba35a70e8c01c5b15722a86118e53f
yu-ki <webmaster@horrorgame.net>
2018-01-04 15:17:56 +0900

    amazonの画像はみだしを修正

--- sources/web/resources/views/game/common/shop.blade.php
+++ sources/web/resources/views/game/common/shop.blade.php
@@ -1,5 +1,5 @@
 @switch ($shopId)
     @case (\Hgs3\Constants\Game\Shop::AMAZON)
-    <a href="{{ $shopUrl }}" target="_blank"><img src="{{ url('img/assocbutt_or_detail._V371070159_.png') }}"></a>
+    <a href="{{ $shopUrl }}" target="_blank"><img src="{{ url('img/assocbutt_or_detail._V371070159_.png') }}" style="max-width:100%;"></a>
     @break
 @endswitch
