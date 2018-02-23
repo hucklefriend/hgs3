1d14c0ace548063b5140c991625b10377b3da018
yu-ki <webmaster@horrorgame.net>
2018-02-23 16:41:12 +0900

    セッションの設定を修正

--- sources/web/config/app.php
+++ sources/web/config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/hgn'),
+    'url' => env('APP_URL', 'https://horrorgame.net/'),
 
     /*
     |--------------------------------------------------------------------------
