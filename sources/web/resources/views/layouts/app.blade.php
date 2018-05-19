f60d5a24122d6bbf1106972efcce86ad5d886864
huckle <webmaster@horrorgame.net>
2018-05-19 19:33:52 +0900

    細かい修正

--- sources/web/resources/views/layouts/app.blade.php
+++ sources/web/resources/views/layouts/app.blade.php
@@ -4,7 +4,11 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-        <title>@yield('title') {{ env('APP_NAME') }} (β)</title>
+        @if (View::hasSection('title'))
+        <title>@yield('title') | {{ env('APP_NAME') }} (β)</title>
+        @else
+        <title>{{ env('APP_NAME') }} (β)</title>
+        @endif
 
         <link rel="stylesheet" href="{{ url('vendors/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css') }}">
         <link rel="stylesheet" href="{{ url('vendors/bower_components/animate.css/animate.min.css') }}">
