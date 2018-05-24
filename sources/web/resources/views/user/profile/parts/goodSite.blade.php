c69f60817d4c3df0e6b31dec65ac635292e7ee9a
yu-ki <webmaster@horrorgame.net>
2018-05-24 12:04:59 +0900

    #10 完成。デザイン面はよく考えた方がいいかも？

--- sources/web/resources/views/user/profile/parts/goodSite.blade.php
+++ sources/web/resources/views/user/profile/parts/goodSite.blade.php
@@ -6,11 +6,21 @@
 @endif
 @foreach ($goodSites as $fs)
     @isset($sites[$fs->site_id])
-        @php $s = $sites[$fs->site_id]; @endphp
+        @php $s = $sites[$fs->site_id]; $u = $users[$s->user_id]; @endphp
     <div class="mb-5">
-    @include('site.common.normal', ['s' => $s, 'u' => $users[$s->user_id]])
+        <div class="mb-2">
+            いいねした日 {{ format_date(strtotime($fs->good_at)) }}
+        </div>
+
+        @include('site.common.minimal', ['s' => $s])
+        <div class="d-flex align-content-start flex-wrap site-info mt-1">
+            <div class="mr-2"><i class="far fa-user"></i> <a href="{{ route('プロフィール', ['showId' => $u->show_id]) }}">{{ $u->name }}</a></div>
+            @if ($s->updated_timestamp > 0)
+                <div><i class="fas fa-redo-alt"></i> {{ format_date($s->updated_timestamp) }}</div>
+            @endif
+        </div>
     </div>
-    @endif
+    @endisset
 @endforeach
 
 @include('common.pager', ['pager' => $goodSites])
