f0484e8529c611fea2a92573cb70889e37d033e9
yu-ki <webmaster@horrorgame.net>
2018-06-27 18:59:39 +0900

    ゲームデータ追加

--- sources/web/storage/master/20180701/official_site/insert.php
+++ sources/web/storage/master/20180701/official_site/insert.php
@@ -1,4 +1,7 @@
 <?php
 return [
     ['soft_id' => '291','title' => '公式サイト','url' => 'https://se-ec.co.jp/application-developer-dept/iosandroid-%E8%84%B1%E5%87%BA%E3%82%B2%E3%83%BC%E3%83%A0%E3%80%8E%E5%BF%83%E9%9C%8A%E6%97%85%E9%A4%A8%E3%81%8B%E3%82%89%E3%81%AE%E8%84%B1%E5%87%BA%E3%80%8F%E3%82%92%E3%83%AA%E3%83%AA%E3%83%BC/','priority' => '1'],
+
+    ['soft_id' => '373','title' => '公式サイト','url' => 'http://jp.console.oasgames.com/game/DYING:%20Reborn','priority' => '1'],
+    ['soft_id' => '373','title' => '<i class="fab fa-playstation"></i>公式','url' => 'https://www.jp.playstation.com/games/dying-reborn-ps4/','priority' => '2'],
 ];
