02113caa649c82fe9ca689e1ba4daa5afcd40733
huckle <webmaster@horrorgame.net>
2018-07-04 23:43:56 +0900

    sample image

--- sources/web/resources/views/friend/index.blade.php
+++ sources/web/resources/views/friend/index.blade.php
@@ -9,29 +9,72 @@
             <h1>フレンド探し</h1>
         </header>
 
-        <div class="card card-hgn d-none">
+        <div class="card card-hgn">
             <div class="card-body">
-                <h2 class="card-title mb-0">検索フォーム <button id="search_form_open" class="btn btn-sm btn-secondary">open</button></h2>
+                <h2 class="card-title mb-0">検索フォーム<button id="search_form_open" class="ml-3 btn btn-sm btn-secondary">open</button></h2>
                 <div id="search" style="display:none;">
-                    <form class="mt-3">
-                        <form method="GET" action="{{ route('フレンド') }}">
-                            <div class="form-group">
-                                <div>
-                                    <label for="attribute" class="hgn-label"><i class="fas fa-check"></i> 属性</label>
+                    <form method="GET" action="{{ route('フレンド') }}" class="mt-2">
+                        <div class="form-group">
+                            <div>
+                                <label for="attribute" class="hgn-label"><i class="fas fa-check"></i> 属性</label>
+                            </div>
+                            <div class="d-flex flex-wrap">
+                                @foreach (\Hgs3\Constants\User\Attribute::$text as $attrId => $attrName)
+                                    <div class="btn-group-toggle my-2 mr-2" data-toggle="buttons">
+                                        <label class="custom-control custom-checkbox text-left btn hgn-check-btn">
+                                            <input type="checkbox" class="custom-control-input" id="attr_{{ $attrId }}" name="attr[]" value="{{ $attrId }}" autocomplete="off"{{ checked(in_array($attrId, []), true) }}>
+                                            <span class="custom-control-indicator"></span>
+                                            <span class="custom-control-description">{{ $attrName }}</span>
+                                        </label>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="form-help"></div>
+                        <div class="form-group">
+                            <div>
+                                <label for="attribute" class="hgn-label"><i class="fas fa-check"></i> 公開している外部サービス</label>
+                            </div>
+                            <div class="d-flex flex-wrap">
+                                <div class="btn-group-toggle my-2 mr-2" data-toggle="buttons">
+                                    <label class="custom-control custom-checkbox text-left btn hgn-check-btn">
+                                        <input type="checkbox" class="custom-control-input" name="sns[]" value="{{ \Hgs3\Constants\SocialSite::TWITTER }}" autocomplete="off"{{ checked(in_array($attrId, []), true) }}>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">{{ \Hgs3\Constants\SocialSite::getIconAndName(\Hgs3\Constants\SocialSite::TWITTER) }}</span>
+                                    </label>
                                 </div>
-                                <div class="d-flex flex-wrap">
-                                    @foreach (\Hgs3\Constants\User\Attribute::$text as $attrId => $attrName)
-                                        <div class="btn-group-toggle my-2 mr-2" data-toggle="buttons">
-                                            <label class="custom-control custom-checkbox text-left btn hgn-check-btn">
-                                                <input type="checkbox" class="custom-control-input" id="attr_{{ $attrId }}" name="attr[]" value="{{ $attrId }}" autocomplete="off"{{ checked(in_array($attrId, []), true) }}>
-                                                <span class="custom-control-indicator"></span>
-                                                <span class="custom-control-description">{{ $attrName }}</span>
-                                            </label>
-                                        </div>
-                                    @endforeach
+                                <div class="btn-group-toggle my-2 mr-2" data-toggle="buttons">
+                                    <label class="custom-control custom-checkbox text-left btn hgn-check-btn">
+                                        <input type="checkbox" class="custom-control-input" name="sns[]" value="{{ \Hgs3\Constants\SocialSite::FACEBOOK }}" autocomplete="off"{{ checked(in_array($attrId, []), true) }}>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">{{ \Hgs3\Constants\SocialSite::getIconAndName(\Hgs3\Constants\SocialSite::FACEBOOK) }}</span>
+                                    </label>
+                                </div>
+                                <div class="btn-group-toggle my-2 mr-2" data-toggle="buttons">
+                                    <label class="custom-control custom-checkbox text-left btn hgn-check-btn">
+                                        <input type="checkbox" class="custom-control-input" name="sns[]" value="{{ \Hgs3\Constants\SocialSite::GOOGLE_PLUS }}" autocomplete="off"{{ checked(in_array($attrId, []), true) }}>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">{{ \Hgs3\Constants\SocialSite::getIconAndName(\Hgs3\Constants\SocialSite::GOOGLE_PLUS) }}</span>
+                                    </label>
+                                </div>
+                                <div class="btn-group-toggle my-2 mr-2" data-toggle="buttons">
+                                    <label class="custom-control custom-checkbox text-left btn hgn-check-btn">
+                                        <input type="checkbox" class="custom-control-input" name="sns[]" value="{{ \Hgs3\Constants\SocialSite::GITHUB }}" autocomplete="off"{{ checked(in_array($attrId, []), true) }}>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">{{ \Hgs3\Constants\SocialSite::getIconAndName(\Hgs3\Constants\SocialSite::GITHUB) }}</span>
+                                    </label>
+                                </div>
+                                <div class="btn-group-toggle my-2 mr-2" data-toggle="buttons">
+                                    <label class="custom-control custom-checkbox text-left btn hgn-check-btn">
+                                        <input type="checkbox" class="custom-control-input" name="sns[]" value="{{ \Hgs3\Constants\SocialSite::PIXIV }}" autocomplete="off"{{ checked(in_array($attrId, []), true) }}>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">{{ \Hgs3\Constants\SocialSite::getIconAndName(\Hgs3\Constants\SocialSite::PIXIV) }}</span>
+                                    </label>
                                 </div>
                             </div>
-                            <div class="form-help"></div>
+                        </div>
+                        <div class="form-help"></div>
+                        <button class="btn btn-secondary">検索</button>
                     </form>
                 </div>
             </div>
