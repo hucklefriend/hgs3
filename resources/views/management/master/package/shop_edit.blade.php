30cab52c56cec1bdbd240c632cf3c533d0fd2315
yuki <webmaster@horrorgame.net>
2022-02-14 06:09:04 +0900

    ゲームパッケージのショップ

--- resources/views/management/master/package/shop_edit.blade.php
+++ resources/views/management/master/package/shop_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.management')
 
-@section('title', 'パッケージの販売ショップ登録')
+@section('title', 'パッケージの販売ショップ更新')
 
 @section('content')
     <ol class="breadcrumb float-xl-end">
@@ -8,21 +8,22 @@
         <li class="breadcrumb-item">マスター</li>
         <li class="breadcrumb-item"><a href="{{ route('管理-マスター-パッケージ') . session('search_package') }}">パッケージ</a></li>
         <li class="breadcrumb-item"><a href="{{ route('管理-マスター-パッケージ詳細', $package) }}">詳細</a></li>
-        <li class="breadcrumb-item active">販売ショップ登録</li>
+        <li class="breadcrumb-item active">販売ショップ更新</li>
     </ol>
-    <h1 class="page-header">販売ショップ登録</h1>
+    <h1 class="page-header">販売ショップ更新</h1>
     <div class="panel panel-inverse">
         <div class="panel-heading">
-            <h4 class="panel-title">新規登録</h4>
+            <h4 class="panel-title">更新</h4>
         </div>
-        <form method="POST" action="{{ route('管理-マスター-パッケージショップ追加処理', $package) }}">
+        <form method="POST" action="{{ route('管理-マスター-パッケージショップ更新処理', ['package' => $package, 'shop' => $model]) }}">
+            {{ method_field('PUT') }}
             {{ csrf_field() }}
 
             <div class="panel-body">
                 @include('management.master.package.shop_form')
             </div>
             <div class="panel-footer text-end">
-                <button type="submit" class="btn btn-default">登録</button>
+                <button type="submit" class="btn btn-default">更新</button>
             </div>
         </form>
     </div>
